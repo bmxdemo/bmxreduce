c433335058079244ccf7b81348e5ccab244e0f93
Christopher Sheehy <chris.d.sheehy@gmail.com>
2017-10-23 18:45:54 -0400

    new plotting

--- bmxreduce/reduce_plot.py
+++ bmxreduce/reduce_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import reduce_init
+from astropy.time import Time
 
 plt.ioff()
 
@@ -9,7 +10,7 @@ def nanhist(x, **kwargs):
 
 class genplots():
     
-    def __init__(self, r):
+    def __init__(self, r, fext=''):
         """Takes reduce_init.reduce object as input, generates reduc plots"""
 
         # Reduced data
@@ -18,29 +19,61 @@ class genplots():
         # Choose this (must be float)
         self.dpi = 80.0
 
+        # Get useful info
+        self.fmin = self.r.d.freq[0][0]
+        self.fmax = self.r.d.freq[0][-1]
+        self.tmin = 0
+        self.tmax = self.r.d.deltaT[0]*self.r.d.nSamples / 60. # minutes
+
+        self.ts = Time(self.r.d.data['mjd'][0],  format='mjd')
+        self.te = Time(self.r.d.data['mjd'][-1], format='mjd')
+
+        # File naming
+        self.filebase = self.r.tag
+
         # Close all open plots
         plt.close('all')
 
         return
 
-    def plotrawdata(self, chan):
+    def plotrawdata(self, dochan=None, fext=''):
         """Plot raw data, maksed and unmasked. Takes chan index."""
-        
-        chn = self.r.getchname(chan)
-        v = self.r.d.data[chn]
-        
-        # Size of data
-        sz = np.array(v.shape)
 
-        # Let's make a figure of the right size with maybe some padding all around
-        padx = 0.025
-        pady = 0.1
-        figsz = (1+np.array([padx,pady]))*sz/self.dpi
-        fig = plt.figure(figsize=figsz, dpi=self.dpi)
-        
-        ax = plt.Axes(fig, [padx/2, pady/2, 1-padx, 1-pady])
-        fig.add_axes(ax)
-        ax.imshow(v.T)
-        plt.savefig('test.png', dpi=self.dpi)
+        if dochan is None:
+            dochan = range(self.r.d.nChan)
+        elif ~iterable(dochan):
+            dochan = [dochan]
 
-        plt.close(fig)
+        for chan in dochan:
+
+            chn = self.r.getchname(chan)
+            v = self.r.d.data[chn]
+
+            # Size of data
+            sz = np.array(v.shape)
+
+            # Let's make a figure of a standard size but with hugely increased
+            # dpi. This will hopefully keep labels looking readable when zoomed
+            # out. 
+            padx = 0.2
+            pady = 0.25
+            figsz = (1+np.array([padx,pady]))*sz/self.dpi
+            fac = 20.0
+            fig = plt.figure(figsize=figsz/fac, dpi=self.dpi*fac)
+
+            ax = plt.Axes(fig, [padx/2 - padx/4, pady/2+.02, 1-padx+padx/4, 1-pady])
+            fig.add_axes(ax)
+            imax = ax.imshow(np.log10(v.T), extent=(self.tmin,self.tmax,self.fmax,self.fmin))
+            ax.grid('on')
+            imax.axes.set_xlabel('t (minutes)');
+            imax.axes.set_ylabel('freq (MHz)');
+            imax.axes.set_title('raw adc (log10[ADU^2]) -- {:s} - {:s} (UTC)'.format(self.ts.iso,self.te.iso))
+
+            cpad = 0.01
+            cbaxes = fig.add_axes([1-padx/2+cpad, pady/2, padx/2-cpad-padx/4-.02, 1-pady]) 
+            cax = fig.colorbar(imax, cax=cbaxes)
+
+            fname = self.filebase + '_'+chn+'_wfraw'+fext+'.png'
+
+            plt.savefig(fname, dpi=self.dpi*fac)
+            plt.close(fig)
