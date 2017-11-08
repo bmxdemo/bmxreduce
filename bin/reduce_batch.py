580b2e4d0f2b6951b73b0a2815bed619b4fcdfdf
Christopher Sheehy <chris.d.sheehy@gmail.com>
2017-11-07 21:50:33 -0500

    script to run when farming to nodes

---
+++ bin/reduce_batch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+sys.path+=['.','..','bmxreduce']
+import bmxreduce as br
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-t", dest="tag", type="str", default='')
+(o, args) = parser.parse_args()
+
+r = br.reduce(o.tag)
+r.doreduce()
+
