d248c74f391f9787f40080ede9f9fc8d59e1a12f
BMX Account <bmx@astro0010.rcf.bnl.gov>
2018-01-12 08:58:58 -0500

    fixed scripts

--- daily_cron/daily_cron.sh
+++ daily_cron/daily_cron.sh
@@ -4,8 +4,8 @@ alias wq='wq.exe'
 export BMXDATA=/gpfs01/astro/workarea/bmxdata/raw
 export BMXREDUCED=/gpfs01/astro/workarea/bmxdata/reduced
 export PATH=/gpfs01/astro/packages/anaconda/default/bin:$PATH
-export LOGFN=/direct/astro+u/bmx/daily_cron/logs/`date +%y%m%d`.log
-export LOGFNE=/direct/astro+u/bmx/daily_cron/logs/`date +%y%m%d`.err
+export LOGFN=/direct/astro+u/bmx/bmxreduce/daily_cron/logs/`date +%y%m%d`.log
+export LOGFNE=/direct/astro+u/bmx/bmxreduce/daily_cron/logs/`date +%y%m%d`.err
 echo "-----------------finalize_transfer------------" >$LOGFN 2>$LOGFNE
 /direct/astro+u/bmx/bmxreduce/daily_cron/finalize_transfer.py -v >> $LOGFN 2>$LOGFNE
 echo "-----------------BMXREDUCE--------------------" >>$LOGFN 2>$LOGFNE
