e9f19b8d5cbb2105fe93086474e1bb2b3e308f8a
Dana Zimmer <dmz11@astro0010.rcf.bnl.gov>
2019-01-23 10:51:22 -0500

    only mask first and last data samples, commented out  outlier masking

--- bmxreduce/reduce_init.py
+++ bmxreduce/reduce_init.py
@@ -124,9 +124,9 @@ class reduce(object):
 	# indices now synch correctly (DZ 01/18/19)
         #self.calind = np.roll(ind,1)
         #self.calind[0] = self.calind[1]
-	self.calind = np.zeros(self.d.nSamples)
+	self.calind = np.zeros(self.d.nSamples).astype('bool')
 	wherecal = np.where(ind==128)[0]
-	self.calind[wherecal] = 1	
+	self.calind[wherecal] = True	
 
         dind = self.calind*1.0 - np.roll(self.calind,1)
         chind = np.where(dind != 0)[0]
@@ -275,6 +275,7 @@ class reduce(object):
 
             ###############
             # Mask Outliers
+	    # Want to keep outliers for now (DZ 19/01/22)
             # Loop over frequencies
             for k in range(x.shape[1]):
 
@@ -282,51 +283,51 @@ class reduce(object):
                 maskind = np.zeros(x.shape[0]).astype('bool')
 
                 # Do the masking step this many times
-                nmask = 2
-                for nm in range(nmask):
+                #nmask = 2
+                #for nm in range(nmask):
                 
                     # Loop over cal on and cal off separately
-                    for l in range(2):
+                    #for l in range(2):
 
-                        if l==0:
-                            ci = ~self.calind # Cal off data
-                        else:
-                            ci = self.calind # Cal on data
+                        #if l==0:
+                            #ci = ~self.calind # Cal off data
+                        #else:
+                            #ci = self.calind # Cal on data
 
                         # Mask various outliers in v, dv/dt, etc.
-                        for m in range(2):
+                        #for m in range(2):
 
-                            if m==0:
+                            #if m==0:
                                 # Get data itself
-                                v = x[ci,k]
-                            else:
+                                #v = x[ci,k]
+                            #else:
                                 # Get 1st deriv of data. There are gaps in the data for cal
                                 # on/off but they're short so that's okay.
-                                v = x[ci,k] - np.roll(x[ci,k], 1, axis=0)
-                                v[0] = 0
+                                #v = x[ci,k] - np.roll(x[ci,k], 1, axis=0)
+                                #v[0] = 0
 
                             # Times
-                            t = t0[ci]
+                            #t = t0[ci]
 
                             # Find where data is not already masked
-                            gi = np.where(np.isfinite(v))
-                            gt = t[gi]; gv = v[gi]
-                            if len(gv) < 0.2*len(v):
+                            #gi = np.where(np.isfinite(v))
+                            #gt = t[gi]; gv = v[gi]
+                            #if len(gv) < 0.2*len(v):
                                 # If more than 80% of the data is already NaN,
                                 # mask the whole thing
-                                maskind[ci] = True
-                            else:
+                                #maskind[ci] = True
+                            #else:
                                 # Poly subtract data
-                                p = np.polyfit(gt, gv, 4)
-                                v = v - np.polyval(p, t)
+                                #p = np.polyfit(gt, gv, 4)
+                                #v = v - np.polyval(p, t)
 
                                 # Get std of middle 80th percentile of values
-                                p = np.nanpercentile(v,[10,90])
-                                ind = (v>p[0]) & (v<p[1])
-                                std0 = np.nanstd(v[ind])
+                                #p = np.nanpercentile(v,[10,90])
+                                #ind = (v>p[0]) & (v<p[1])
+                                #std0 = np.nanstd(v[ind])
 
                                 # Mask 5 sigma outliers
-                                maskind[ci] = (maskind[ci]) | (np.abs(v) > 5*std0)
+                                #maskind[ci] = (maskind[ci]) | (np.abs(v) > 5*std0)
 
 
                 # Expand masked data by some number of samples on either side
@@ -335,9 +336,7 @@ class reduce(object):
                 #maskind[maskind != 0] = 1
                 #maskind = maskind.astype('bool')
 
-                # Mask first and last of cal ind
-                maskind[self.ind['sc']] = True
-                maskind[self.ind['ec']] = True
+		# Mask first and last of data ind, where noise diode turns on/off
                 maskind[self.ind['sd']] = True
                 maskind[self.ind['ed']] = True
 
