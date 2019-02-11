cbed32b7b1d5bf69d36ade875be68603ebfa5457
BMX Account <bmx@astro0010.rcf.bnl.gov>
2019-02-10 23:13:37 -0500

    new jobs dont take so much memory

--- bin/reduce.py
+++ bin/reduce.py
@@ -11,7 +11,7 @@ dm = br.datamanager()
 dm.gettags(new=True)
 f = farmit.farmit('bin/reduce_batch.py', args={'t':dm.tags},
                   names=['BMX_'+tag for tag in dm.tags],
-                  reqs={'N':4,'X':0,'priority':'low','mode':'bycore1'})
+                  reqs={'N':2,'X':0,'priority':'low','mode':'bycore1'})
 f.writejobfiles()
 f.runjobs(maxjobs=500)
 
